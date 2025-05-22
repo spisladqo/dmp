9d3f7d3aef67e04e30961e431c5ae241561a37cf
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-05-22 21:44:34 +0300

    feat: make stats a field of a dev_handle struct, use goto for errors

--- dmp.c
+++ dmp.c
@@ -21,106 +21,114 @@ typedef struct {
 	unsigned int rrq_num;
 	unsigned int wrq_num;
 	unsigned int rq_num;
-} dmp_stat_t;
-
-static dmp_stat_t stats;
+} dmp_stats_t;
 
 typedef struct {
+	dmp_stats_t* stats;
 	struct dm_dev* dev;
-	sector_t start;
-} dmp_target_t;
+} dmp_dev_handle_t;
 
 static int dmp_map(struct dm_target* ti, struct bio* bio)
 {
 	unsigned long long bsize;
 	unsigned long long avg_bsize;
-	dmp_target_t* dt;
+	dmp_stats_t* stats;
+	dmp_dev_handle_t* dh;
 
-	dt = ti->private;
+	dh = ti->private;
+	stats = dh->stats;
 	bsize = bio->bi_iter.bi_size;
-	bio->bi_bdev = dt->dev->bdev;
-	stats.rq_num++;
-	stats.rq_bsize_total = stats.rq_bsize_total + bsize;
+	bio->bi_bdev = dh->dev->bdev;
+	stats->rq_num++;
+	stats->rq_bsize_total = stats->rq_bsize_total + bsize;
 
-	pr_info("Requests num: %u\n", stats.rq_num);
+	pr_info("Requests num: %u\n", stats->rq_num);
 	pr_info("Received bio with block of size: %llu\n", bsize);
-	pr_info("Total block size: %llu\n", stats.rq_bsize_total);
+	pr_info("Total block size: %llu\n", stats->rq_bsize_total);
 
 	switch (bio_op(bio)) {
 	case REQ_OP_READ:
-		stats.rrq_num++;
-		stats.rrq_bsize_total += bsize;
-		avg_bsize = stats.rrq_bsize_total / stats.rrq_num;
-		pr_info("Current read requests num: %u\n", stats.rrq_num);
+		stats->rrq_num++;
+		stats->rrq_bsize_total += bsize;
+		avg_bsize = stats->rrq_bsize_total / stats->rrq_num;
+		pr_info("Current read requests num: %u\n", stats->rrq_num);
 		pr_info("Current average read block size: %llu\n", avg_bsize);
 		break;
 	case REQ_OP_WRITE:
-		stats.wrq_num++;
-		stats.wrq_bsize_total += bsize;
-		avg_bsize = stats.wrq_bsize_total / stats.wrq_num;
-		pr_info("Current write requests num: %u\n", stats.wrq_num);
+		stats->wrq_num++;
+		stats->wrq_bsize_total += bsize;
+		avg_bsize = stats->wrq_bsize_total / stats->wrq_num;
+		pr_info("Current write requests num: %u\n", stats->wrq_num);
 		pr_info("Current average write block size: %llu\n", avg_bsize);
 		break;
 	default:
 		break;
 	}
 
-	submit_bio(bio);
+	bio_set_dev(bio, dh->dev->bdev);
 
-	pr_info("Bio submitted successfully\n");
 	return DM_MAPIO_SUBMITTED;
 }
 
 static int dmp_ctr(struct dm_target* ti, unsigned int argc, char** argv)
 {
-	dmp_target_t* dt;
-	sector_t start;
+	dmp_dev_handle_t* dh;
 	int error;
 
-	if (argc != 2) {
-		pr_err("Invalid number of arguments\n");
+	pr_info("argc: %u, argv[0]: %s\n", argc, argv[0]);
+
+	if (argc != 1) {
 		ti->error = "Invalid number of arguments";
-		return -EINVAL;
+		error = -EINVAL;
+		goto fail;
 	}
 
-	dt = kzalloc(sizeof(dmp_target_t), GFP_KERNEL);
-	if (!dt) {
-		pr_err("Cannot allocate memory for dt\n");
-		ti->error = "Cannot allocate memory for dt";
-		return -ENOMEM;
+	pr_info("allocating dh...\n");
+
+	dh = kzalloc(sizeof(dmp_dev_handle_t), GFP_KERNEL);
+	if (!dh) {
+		ti->error = "Cannot allocate memory for dmp device handle";
+		error = -ENOMEM;
+		goto fail;
 	}
 
-	if (sscanf(argv[1], "%llu", &start) != 1)
-	{
-		pr_err("Invalid device sector\n");
-		ti->error = "Invalid device sector";
-		kfree(dt);
-		return -EINVAL;
+	dh->stats = kzalloc(sizeof(dmp_stats_t), GFP_KERNEL);
+	if (!dh->stats) {
+		ti->error = "Cannot allocate memory for dh";
+		error = -ENOMEM;
+		goto fail_stats;
 	}
 
-	error = dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &(dt->dev));
+	pr_info("dh allocated\n");
+	pr_info("getting device...\n");
+
+	error = dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &(dh->dev));
 	if (error) {
-		pr_err("Device lookup failed\n");
 		ti->error = "Device lookup failed";
-		kfree(dt);
-		return error;
+		goto fail;
 	}
 
-	dt->start = start;
-	ti->private = dt;
+	ti->private = dh;
+	pr_info("got device!\n");
 
 	return 0;
+
+fail_stats:
+	kfree(dh->stats);
+fail:
+	kfree(dh);
+	return error;
 }
 
 static void dmp_dtr(struct dm_target* ti)
 {
-	dmp_target_t* dt = ti->private;
-	dm_put_device(ti, dt->dev);
-	kfree(dt);
+	dmp_dev_handle_t* dh = ti->private;
+	dm_put_device(ti, dh->dev);
+	kfree(dh);
 }
 
 static struct target_type dmp_target = {
-	.name = "dmp_target",
+	.name = "dmp",
 	.version = {1, 0, 0},
 	.module = THIS_MODULE,
 	.map = dmp_map,
@@ -133,9 +141,12 @@ static int __init dmp_init(void)
 	int result = dm_register_target(&dmp_target);
 	if (result) {
 		pr_err("Failed to register target: error %d\n", result);
+		dm_unregister_target(&dmp_target);
+		return result;
 	}
+
 	pr_info("Initialized dmp module\n");
-	return result;
+	return 0;
 }
 
 static void __exit dmp_exit(void)
